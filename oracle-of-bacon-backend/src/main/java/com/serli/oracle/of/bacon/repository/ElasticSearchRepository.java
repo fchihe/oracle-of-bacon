8af47b10b1cc38bfe67c3a8829f95bfb47b6f156
Xavier Huberdeau <xavier.huberdeau@gmail.com>
2017-01-17 15:13:08 +0100

    Implement elastic suggest + fix frontend suggest event
    - in the front end, the use of keypress didn't pass the last letter of the search. Keyup does

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -2,35 +2,69 @@ package com.serli.oracle.of.bacon.repository;
 
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.SearchResult.Hit;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.gson.JsonObject;
 
 public class ElasticSearchRepository {
 
-    private final JestClient jestClient;
+	private final JestClient jestClient;
 
-    public ElasticSearchRepository() {
-        jestClient = createClient();
+	public ElasticSearchRepository() {
+		jestClient = createClient();
 
-    }
+	}
 
-    public static JestClient createClient() {
-        JestClient jestClient;
-        JestClientFactory factory = new JestClientFactory();
-        factory.setHttpClientConfig(new HttpClientConfig.Builder("http://localhost:9200")
-                .multiThreaded(true)
-                .readTimeout(60000)
-                .build());
+	public static JestClient createClient() {
+		JestClient jestClient;
+		JestClientFactory factory = new JestClientFactory();
+		factory.setHttpClientConfig(new HttpClientConfig.Builder("http://localhost:9200")
+				.multiThreaded(true)
+				.readTimeout(60000)
+				.build());
 
-        jestClient = factory.getObject();
-        return jestClient;
-    }
+		jestClient = factory.getObject();
+		return jestClient;
+	}
 
-    public List<String> getActorsSuggests(String searchQuery) {
-        // TODO implent suggest
-        return null;
-    }
+	public List<String> getActorsSuggests(String searchQuery) {
+		String query = "{\n" +
+					   "	\"query\": {\n" +
+					   "    	\"match\" : {\n"+
+					   "       		\"name\" : {\n"+
+					   "       			\"query\" : \""+searchQuery+"\",\n"+
+					   "       			\"fuzziness\" : \"1\",\n"+
+					   "       			\"operator\" : \"and\"\n "+
+					   "        	}\n" +
+					   "        }\n" +
+					   "    }\n" +
+					   "}";
+
+		Search search = new Search.Builder(query)
+				.addIndex("people")
+				.addType("actor")
+				.build();
+		try {
+			SearchResult result = jestClient.execute(search);
+			List<String> actorNames = result.getHits(JsonObject.class)
+					.stream()
+					.map(hit -> hit.source.get("name").getAsString())
+					.collect(Collectors.toList());
+			return actorNames;
+		} catch (IOException e) {
+			System.out.println("Error while suggesting actor names. Error is : ");
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 
 }
