d564a8fe27f4c1ca0e778247b2b6b9b647457fe6
Xavier Huberdeau <xavier.huberdeau@gmail.com>
2017-01-17 11:03:19 +0100

    Update gitignore + implement last10Searches
    - ignore files generated by eclipse
    - implement last10searches, add method to push an actors search (First In First Out)

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
@@ -2,9 +2,29 @@ package com.serli.oracle.of.bacon.repository;
 
 import java.util.List;
 
+import redis.clients.jedis.Jedis;
+
 public class RedisRepository {
-    public List<String> getLastTenSearches() {
-        // TODO implement last 10 searchs
-        return null;
+	
+	private Jedis jedis;
+	
+    public RedisRepository() {
+    	jedis = new Jedis("localhost");
+	}
+
+    /**
+     * Get the first 10 items in the list of actors searched
+     * @return
+     */
+	public List<String> getLastTenSearches() {
+        return jedis.lrange("actorSearches", 0, 9);
     }
+	
+	/**
+	 * Add the actor name to the beginning the actors searched list
+	 * @param actorName
+	 */
+	public void addSearch(String actorName){
+		jedis.lpush("actorSearches", actorName);
+	}
 }
